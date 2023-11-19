7fd80715a73b465ced89fcb585a778e988d4fd29
haich0ng <shanghaihaze@hotmail.com>
2023-11-19 16:03:12 +0000

    all

--- Monster.java
+++ Monster.java
@@ -1,9 +1,12 @@
+import java.lang.Math;
+
 public class Monster {
     private String name;
     private int attack; //how much the player hurt from the monster
     private float sense; //how easily can the monster discover the player
+    //from a scale of 0 to 10
     /**
-     * Create a room described "description". Initially, it has
+     * Create a Monster class
      * no exits. "description" is something like "a kitchen" or
      */
     public Monster(String nam, int att, float sne){
@@ -12,4 +15,13 @@ public class Monster {
         sense = sne;
     }
 
+    public boolean alarm(int playerstat) {
+        double temp = 0.5 - (playerstat - sense) * 0.05;
+        //a probobility of player being discovered
+        if (temp > Math.random()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
